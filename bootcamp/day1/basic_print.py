f9dca6bd050628478a12f6931d86792ccf4fc2d0
crypt3d.c00kie <cookie.crypted@gmail.com>
2022-03-07 20:57:13 +0530

    creation of day1

---
+++ bootcamp/day1/basic_print.py
@@ -0,0 +1,7 @@
+#first ever line of python code
+#starts from here but i know its not gonna end here
+#day1
+
+print("The Day -1 ")
+print("First ever line of python code")
+print("It starts here but its not gonna end here")
