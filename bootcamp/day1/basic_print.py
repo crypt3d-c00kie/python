46ffcd7e87e075b789ae3691c89a2adb50d06d06
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-05-03 22:50:55 +0530

    more git commit

--- bootcamp/day1/basic_print.py
+++ bootcamp/day1/basic_print.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #first ever line of python code
 #starts from here but i know its not gonna end here
 #day1
@@ -9,15 +8,4 @@ myname = input("Your name : ")
 print("Hello" + " " + "Cookie")
 print("Have a nice day! " + myname)
 
-=======
-#first ever line of python code
-#starts from here but i know its not gonna end here
-#day1
-
-print("Hello world!!")
-myname = input("Your name : ")
-
-print("Hello" + " " + "Cookie")
-print("Have a nice day! " + myname)
-
->>>>>>> 4ac856524fa20ff777965ae20837f5e96e149599
+print("End of file")
