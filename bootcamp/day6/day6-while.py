bc0f01c298588cc3f35ba2eaa00590a1ec375385
crypt3d-c00kie <56737954+crypt3d-c00kie@users.noreply.github.com>
2022-08-01 02:53:29 +0530

    Add files via upload

---
+++ bootcamp/day6/day6-while.py
@@ -0,0 +1,10 @@
+#day6
+#basically a slow day
+#indentation and while loops
+
+import random
+
+print("Welcome to this infinite loop!!")
+while (1):
+    myrandom = random.randint(0,9999)
+    print(myrandom)
