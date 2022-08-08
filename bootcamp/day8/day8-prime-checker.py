3eea0403fbe5c9c9a8043fcf796db82a3aee2f49
crypt3d-c00kie <56737954+crypt3d-c00kie@users.noreply.github.com>
2022-08-08 12:59:02 +0530

    Add files via upload

---
+++ bootcamp/day8/day8-prime-checker.py
@@ -0,0 +1,16 @@
+#day8
+#check a number if its prime
+
+#prime number can only divided by itself and 1
+def prime_check(val):
+    isPrime = True
+    for i in range(2,val):
+        if val  %i == 0:
+            isPrime = False
+    if isPrime:
+        print("It's a prime number")
+    else:
+        print("non-prime number")
+
+n = int(input("Enter the number to check if prime : "))
+prime_check(n)
