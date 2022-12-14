6270be2a2b5b35bc97cb9391a820dc0c75c4a936
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-12-15 00:49:35 +0530

    Add files via upload

---
+++ bootcamp/day9/day9-blind-auction.py
@@ -0,0 +1,30 @@
+
+import os
+
+def findMaxBid(bidDict):
+    #bidDict = {"Alicia" : 222, "Jamie" : 121}
+    maxBid = 0
+    for bidder in bidDict:
+        if bidDict[bidder] > maxBid:
+            maxBid = bidDict[bidder]
+            winner = bidder
+    print(f"The Winner is {winner} with the highest bid of ${maxBid}")
+    
+def main():
+    print("Welcome to the secret auction!")
+    bids = {}
+    finish = False
+    while not finish:
+        name = input('What is your name? :: ')
+        money = int(input('What is your bid? :: $'))
+        bids[name] = money
+        
+        choice = input('Are there any other bidders? Type "Yes" or "No" :: ')
+        if choice.lower() == "no":
+            finish = True
+            findMaxBid(bids)
+        else:
+            os.system('cls')
+        
+if __name__ == '__main__':
+    main()
