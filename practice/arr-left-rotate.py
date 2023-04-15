ee06961e10a2b50d7b60c548073a598461f650c5
crypt3d-c00kie <cookie.crypted@gmail.com>
2023-04-16 02:18:01 +0530

    Add files via upload

---
+++ practice/arr-left-rotate.py
@@ -0,0 +1,22 @@
+# rotate the array by n
+# direction - left
+
+def main():
+    given = [1,2,4,5,6]
+    kIter = [1,2,3,5]
+
+    # for k in kIter:
+    #     given[:] = given[k:] + given[:k]
+    #     print(given)
+
+    
+    for k in kIter:
+        for i in range(k):
+            given.append(given[0])
+            del given[0]
+        
+        print(given)
+
+
+if __name__ == '__main__':
+    main()
